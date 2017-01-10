ab84453df0a7e4c48ebac6589402358a4f141378
Felix Moessbauer <felix.moessbauer@gmail.com>
2017-01-10 10:55:44 +0100

    Use c++11 techniques and RAII for memory management in ff example

--- examples/cpp/ff/main.cpp
+++ examples/cpp/ff/main.cpp
@@ -2,26 +2,24 @@
 #include"frequentflyer.h"
 #include"silverfrequentflyer.h"
 #include"goldfrequentflyer.h"
-#include<iostream>
+#include<vector>
+#include<memory>
 using namespace std;
 
 int main()
 {
-	BaseFrequentFlyer* f[3];
-	
-	f[0] = new FrequentFlyer ("Cornelius Doe", 23166841, 100);
-	f[1] = new SilverFrequentFlyer ("Lucius Doe", 94565432, 100, 5);
-	f[2] = new GoldFrequentFlyer("Rufus Doe", 32155994, 100, 100);
+  vector<unique_ptr<BaseFrequentFlyer>> f;
+  f.reserve(3);
 
-	f[0]->addMiles(100);
-	f[1]->addMiles(100);
-	f[2]->addMiles(100);
+  f.emplace_back(unique_ptr<FrequentFlyer>(new FrequentFlyer("Cornelius Doe", 23166841, 100)));
+  f.emplace_back(unique_ptr<SilverFrequentFlyer>(new SilverFrequentFlyer("Lucius Doe", 94565432, 100, 5)));
+  f.emplace_back(unique_ptr<GoldFrequentFlyer>(new GoldFrequentFlyer("Rufus Doe", 32155994, 100, 100)));
 
-	f[0]->printData();
-	f[1]->printData();
-	f[2]->printData();
-	
-	delete f[0];
-	delete f[1];
-	delete f[2];
+  f[0]->addMiles(100);
+  f[1]->addMiles(100);
+  f[2]->addMiles(100);
+
+  f[0]->printData();
+  f[1]->printData();
+  f[2]->printData();
 }
